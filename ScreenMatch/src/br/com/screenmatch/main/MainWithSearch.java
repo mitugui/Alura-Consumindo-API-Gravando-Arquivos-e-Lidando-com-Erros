a80099baf620e93c29f5494f0a8bc7f5d6782ac3
mitugui <cristian.mitugui04@gmail.com>
2024-03-22 21:38:13 -0300

    Refinando a busca

--- ScreenMatch/src/br/com/screenmatch/main/MainWithSearch.java
+++ ScreenMatch/src/br/com/screenmatch/main/MainWithSearch.java
@@ -6,16 +6,25 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
 
 public class MainWithSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um filme para buscar:");
+        var search = sc.nextLine();
+
+        String address = "http://www.omdbapi.com/?t=" + search + "&apikey=47804e78";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=La+La+Land&apikey=47804e78"))
+                .uri(URI.create(address))
                 .build();
         HttpResponse<String> response = client
                 .send(request, BodyHandlers.ofString());
 
         System.out.println(response.body());
+        
+        sc.close();
     }
 }
